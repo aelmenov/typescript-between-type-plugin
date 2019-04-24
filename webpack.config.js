e3490b90f6b8140cd53016a01725f10a6f67818f
Alex Elmenov <alex.elmenov@gmail.com>
2019-04-25 01:46:27 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             {
                 test: /\.ts$/,
                 loader: "babel-loader",
-                exclude: [ "node_modules" ]
+                exclude: [ "./node_modules" ]
             }
         ]
     },
