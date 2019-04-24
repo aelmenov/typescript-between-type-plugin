cf527986efe95922b27525765994cad1b741c653
Alex Elmenov <alex.elmenov@gmail.com>
2019-04-24 04:02:30 +0300

    Added lib.d.ts

---
+++ lib.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Type difinition for nrange type.
+ *
+ * First generic type is the start of number range.
+ *
+ * Last generic type is the end of number range.
+ */
+declare type nrange<S extends number, E extends number> = number;
